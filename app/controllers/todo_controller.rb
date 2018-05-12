c75bc7f420e7b4103e897f6839183c783dd94f78
Lloyd Empedrado <lloydempedrado02@gmail.com>
2018-05-12 15:23:07 +0000

    todo app

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,6 +1,9 @@
 class TodoController < ApplicationController
     def index
+        @title="Workshop To-do List"
     end
-    def show
+    def show 
+        @todo_count=4
+        @title="Workshop To-do List"
     end
 end
