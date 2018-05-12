48fea20bd4040cd20a0bb61cc2a76b10b6f00497
Lloyd Empedrado <lloydempedrado02@gmail.com>
2018-05-12 17:24:36 +0000

    todo app

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -5,5 +5,11 @@ class TodoController < ApplicationController
     def show 
         @todo_count=4
         @title="Workshop To-do List"
+        todo_id=[:id]
+        if todo_id='1'
+            @todo_description='Make the curriculum'
+        elsif todo_id='2'
+            @todo_description='Buy workshop supplies'
+        end    
     end
 end
