212b1952628682758ac62f8e4ee94699ea0e87a9
Lloyd Empedrado <lloydempedrado02@gmail.com>
2018-05-12 17:39:25 +0000

    todo app

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,13 +3,22 @@ class TodoController < ApplicationController
         @title="Workshop To-do List"
     end
     def show 
-        @todo_count=4
         @title="Workshop To-do List"
-        todo_id=[:id]
-        if todo_id='1'
+        todo_id=params[:id]
+        if todo_id=='1'
             @todo_description='Make the curriculum'
-        elsif todo_id='2'
+        elsif todo_id=='2'
             @todo_description='Buy workshop supplies'
+        elsif todo_id=='3'
+            @todo_description='Meet with the volunteer trainers'
+        elsif todo_id=='4'
+            @todo_description='Order food for Saturday and Sunday'
+        elsif todo_id=='5'
+            @todo_description='Check pre-work assignments'
+        elsif todo_id=='6'
+            @todo_description='Send workshop location to all the students'
+        elsif todo_id=='7'
+            @todo_description='Have a great workshop'
         end    
     end
 end
