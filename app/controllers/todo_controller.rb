7ddd353e61982f26ea980d20e5f6d26d4677f637
Lloyd Empedrado <lloydempedrado02@gmail.com>
2018-05-13 15:49:04 +0000

    create db

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -4,30 +4,12 @@ class TodoController < ApplicationController
     end
     def show 
         @title="Workshop To-do List"
-        todo_id=params[:id] 
-        if todo_id=='1'
-            @todo_description='Make the curriculum'
-            @todo_count='4'
-        elsif todo_id=='2'
-            @todo_description='Buy workshop supplies'
-            @todo_count='3'
-        elsif todo_id=='3'
-            @todo_description='Meet with the volunteer trainers'
-            @todo_count='2'
-        elsif todo_id=='4'
-            @todo_description='Order food for Saturday and Sunday'
-            @todo_count='1'
-        elsif todo_id=='5'
-            @todo_description='Check pre-work assignments'
-            @todo_count='2'
-        elsif todo_id=='6'
-            @todo_description='Send workshop location to all the students'
-            @todo_count='1'
-        elsif todo_id=='7'
-            @todo_description='Have a great workshop'
-            @todo_count='0'
-        end 
+        @todo=Todo.find_by_id(params[:id])
+        @todo_description=@todo.description
+        @todo_pomodoro_estimate=@todo.pomodoro_estimate
+    end
     def new
+        
     end
     def create
          t = Todo.new
@@ -35,6 +17,5 @@ class TodoController < ApplicationController
          t.pomodoro_estimate = params['pomodoro_estimate']
          t.save
          redirect_to "/todo/show/#{ t.id }"
-    end
     end
 end
